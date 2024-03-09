1a339be93b47efb9eabeaea686c7dd261a1cc192
sahil <sahilabcd60@gmail.com>
2024-03-09 18:10:02 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+import { Camera } from "expo-camera";
+import * as MediaLibrary from "expo-media-library";
 
 export default function App() {
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [cameraRef, setCameraRef] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasCameraPermission(status === "granted");
+    })();
+  }, []);
+
+  const takePicture = async () => {
+    if (cameraRef) {
+      const photo = await cameraRef.takePictureAsync({ quality: 0.5 });
+      savePictureToGallery(photo);
+    }
+  };
+
+  const savePictureToGallery = async (photo) => {
+    const { status } = await MediaLibrary.requestPermissionsAsync();
+    if (status === "granted") {
+      const asset = await MediaLibrary.createAssetAsync(photo.uri);
+      MediaLibrary.createAlbumAsync("Selfies", asset)
+        .then(() => {
+          alert('Picture saved to "Selfies" album in your gallery!');
+        })
+        .catch((error) => {
+          console.error("Error creating album:", error);
+        });
+    } else {
+      alert("Permission to access media library denied");
+    }
+  };
+
+  if (hasCameraPermission === null) {
+    return <View style={styles.container} />;
+  }
+  if (hasCameraPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Camera
+        style={styles.camera}
+        type={Camera.Constants.Type.front}
+        ref={(ref) => setCameraRef(ref)}
+      />
+      <Button title="Take a Selfie" onPress={takePicture} />
     </View>
   );
 }
@@ -13,8 +59,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+  },
+  camera: {
+    flex: 1,
   },
 });
